7fdb9727fb98b6520e2311dc75794e8aeed9b57d
raphaelgpalma <raphaelgpalma@icloud.com>
2024-03-19 17:22:12 -0300

    Organizing the file structure

---
+++ src/main/java/com/example/demo/repository/ElementoRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Elemento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ElementoRepository extends JpaRepository<Elemento,Long> {
+
+
+
+
+}
