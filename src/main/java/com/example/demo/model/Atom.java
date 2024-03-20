97cba30b53151896c6616e2ef68d149d0ff7e4a5
Raphael Palma <117413668+raphaelgpalma@users.noreply.github.com>
2024-03-19 21:50:58 -0300

    Update Atom.java

--- src/main/java/com/example/demo/model/Atom.java
+++ src/main/java/com/example/demo/model/Atom.java
@@ -1,4 +1,35 @@
 package com.example.demo.model;
 
-public class Atom {
+import jakarta.persistence.Entity;
+
+@Entity
+public class Atom extends Elemento{
+
+    private int numProtons;
+    private int numEletrons;
+
+    public Atom() {
+    }
+
+    public Atom(String nome, String simboloQuimico, int numeroAtomico, double massaAtomica, String grupo, String periodo) {
+        super(nome, simboloQuimico, numeroAtomico, massaAtomica, grupo, periodo);
+        this.numProtons = numProtons;
+        this.numEletrons = numEletrons;
+    }
+    public int getNumProtons() {
+        return numProtons;
+    }
+
+    public void setNumProtons(int numProtons) {
+        this.numProtons = numProtons;
+    }
+
+    public int getNumEletrons() {
+        return numEletrons;
+    }
+
+    public void setNumEletrons(int numEletrons) {
+        this.numEletrons = numEletrons;
+    }
 }
+
